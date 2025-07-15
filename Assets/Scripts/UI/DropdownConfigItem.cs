d680155c2912d6c4f8098dee409a6a0194e65c6b
csh <csh@smrkn.com>
2025-07-15 23:34:51 +0100

    Add dropdown and numeric slider APIs

---
+++ Assets/Scripts/UI/DropdownConfigItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx.Configuration;
+
+namespace IslandConfig.UI
+{
+    public class EnumDropdownConfigItem<T> : BepInConfigWrapper<T> where T: Enum
+    {
+        public EnumDropdownConfigItem(ConfigEntry<T> configEntry) : base(configEntry)
+        {
+        }
+    }
+
+    public class DropdownConfigItem<T> : BepInConfigWrapper<T> where T : IEquatable<T>
+    {
+        protected readonly IList<T> Options;
+
+        protected DropdownConfigItem(ConfigEntry<T> configEntry) : base(configEntry)
+        {
+            if (configEntry.Description.AcceptableValues is null) return;
+            var constraints = ConfigEntry.Description.AcceptableValues;
+
+            if (constraints is not AcceptableValueList<T> validOptions)
+            {
+                throw new ArgumentException(
+                    $"{nameof(ConfigEntry<T>)} has incorrect constraint type; expected {nameof(AcceptableValueList<T>)}",
+                    nameof(configEntry));
+            }
+
+            Options = new List<T>(validOptions.AcceptableValues);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/NumericSliderConfigItem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using BepInEx.Configuration;
+
+namespace IslandConfig.UI
+{
+    public abstract class NumericSliderConfigItem<T> : BepInConfigWrapper<T> where T : IComparable
+    {
+        internal readonly bool IsWholeNumberType;
+
+        public T MinValue { get; protected set; }
+        public T MaxValue { get; protected set; }
+
+        protected NumericSliderConfigItem(ConfigEntry<T> configEntry, bool isWholeNumberType, T defaultMinValue, T defaultMaxValue) : base(configEntry)
+        {
+            IsWholeNumberType = isWholeNumberType;
+
+            switch (configEntry.Description.AcceptableValues)
+            {
+                case AcceptableValueRange<T> range:
+                    MinValue = range.MinValue;
+                    MaxValue = range.MaxValue;
+                    break;
+                case null:
+                    MinValue = defaultMinValue;
+                    MaxValue = defaultMaxValue;
+                    break;
+                default:
+                    throw new ArgumentException(
+                        $"{configEntry.Definition.Key} has incorrect constraint type, expected {nameof(AcceptableValueRange<T>)}",
+                        nameof(configEntry));
+            }
+        }
+    }
+
+    public class FloatSliderConfigItem : NumericSliderConfigItem<float>
+    {
+        public FloatSliderConfigItem(ConfigEntry<float> configEntry) : base(configEntry, false, float.MinValue,
+            float.MaxValue)
+        {
+        }
+    }
+
+    public class DoubleSliderConfigItem : NumericSliderConfigItem<double>
+    {
+        public DoubleSliderConfigItem(ConfigEntry<double> configEntry) : base(configEntry, false, double.MinValue,
+            double.MaxValue)
+        {
+        }
+    }
+
+    public class DecimalSliderConfigItem : NumericSliderConfigItem<decimal>
+    {
+        public DecimalSliderConfigItem(ConfigEntry<decimal> configEntry) : base(configEntry, false, decimal.MinValue,
+            decimal.MaxValue)
+        {
+        }
+    }
+
+    public class ByteSliderConfigItem : NumericSliderConfigItem<byte>
+    {
+        public ByteSliderConfigItem(ConfigEntry<byte> configEntry) : base(configEntry, true, byte.MinValue,
+            byte.MaxValue)
+        {
+        }
+    }
+
+    public class ShortSliderConfigItem : NumericSliderConfigItem<short>
+    {
+        public ShortSliderConfigItem(ConfigEntry<short> configEntry) : base(configEntry, true, short.MinValue,
+            short.MaxValue)
+        {
+        }
+    }
+
+    public class IntSliderConfigItem : NumericSliderConfigItem<int>
+    {
+        public IntSliderConfigItem(ConfigEntry<int> configEntry) : base(configEntry, true, int.MinValue, int.MaxValue)
+        {
+        }
+    }
+}
