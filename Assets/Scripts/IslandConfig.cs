a558cd83bc7e59ab716114ec55a8a0dad012571e
csh <csh@smrkn.com>
2025-07-16 02:09:57 +0100

    Add WithSlider to ConfigBuilder

--- Assets/Scripts/IslandConfig.cs
+++ Assets/Scripts/IslandConfig.cs
@@ -7,6 +7,7 @@ using BepInEx;
 using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using IslandConfig.UI;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace IslandConfig
@@ -14,18 +15,19 @@ namespace IslandConfig
     public static class IslandConfig
     {
         internal static readonly ConditionalWeakTable<PluginInfo, List<BepInConfigWrapper>> ConfigsByPlugin = new();
-        
+
         private static bool _generatedConfigs;
 
         internal static void GenerateConfigs()
         {
             if (_generatedConfigs) return;
-            
+
             foreach (var pluginInfo in Chainloader.PluginInfos.Values)
             {
                 if (pluginInfo.Instance.Config.Count < 1)
                 {
-                    Debug.Log($"[IslandConfig] Plugin {pluginInfo.Metadata.Name} has no configs to generate entries for");
+                    Debug.Log(
+                        $"[IslandConfig] Plugin {pluginInfo.Metadata.Name} has no configs to generate entries for");
                     continue;
                 }
 
@@ -34,30 +36,34 @@ namespace IslandConfig
                     Debug.Log($"[IslandConfig] Plugin {pluginInfo.Metadata.Name} has already registered configuration");
                     continue;
                 }
-                
+
                 Debug.Log($"[IslandConfig] Generating configs for {pluginInfo.Metadata.Name}");
                 var wrapped = ConfigGenerator.Generate(pluginInfo, pluginInfo.Instance.Config);
                 ConfigsByPlugin.Add(pluginInfo, wrapped.ToList());
             }
-            
+
             _generatedConfigs = true;
         }
-        
+
         public static void Register(Action<ConfigBuilder> build)
         {
             var caller = Assembly.GetCallingAssembly();
 
-            var pluginInfo = Chainloader.PluginInfos.Values.FirstOrDefault(pluginInfos => pluginInfos.Instance.GetType().Assembly == caller);
+            var pluginInfo =
+                Chainloader.PluginInfos.Values.FirstOrDefault(pluginInfos =>
+                    pluginInfos.Instance.GetType().Assembly == caller);
 
             if (pluginInfo == null)
-                throw new InvalidOperationException($"Could not find a BaseUnityPlugin with [BepInPlugin] in {caller.FullName}");
+                throw new InvalidOperationException(
+                    $"Could not find a BaseUnityPlugin with [BepInPlugin] in {caller.FullName}");
 
             var pluginMeta = pluginInfo.Metadata;
-            Debug.Log($"[IslandConfig] Registering config UI for plugin {pluginMeta.Name} ({pluginMeta.GUID}@{pluginMeta.Version})");
-            
+            Debug.Log(
+                $"[IslandConfig] Registering config UI for plugin {pluginMeta.Name} ({pluginMeta.GUID}@{pluginMeta.Version})");
+
             var builder = new ConfigBuilder();
             build(builder);
-            
+
             var list = ConfigsByPlugin.GetOrCreateValue(pluginInfo);
             list.Clear();
             list.AddRange(builder.Configs.Values);
@@ -66,7 +72,9 @@ namespace IslandConfig
         public static void Unregister()
         {
             var caller = Assembly.GetCallingAssembly();
-            var pluginInfo = Chainloader.PluginInfos.Values.FirstOrDefault(pluginInfos => pluginInfos.Instance.GetType().Assembly == caller);
+            var pluginInfo =
+                Chainloader.PluginInfos.Values.FirstOrDefault(pluginInfos =>
+                    pluginInfos.Instance.GetType().Assembly == caller);
             if (pluginInfo is null) return;
             ConfigsByPlugin.Remove(pluginInfo);
         }
@@ -74,9 +82,11 @@ namespace IslandConfig
 
     public class ConfigBuilder
     {
-        internal readonly IDictionary<ConfigEntryBase, BepInConfigWrapper> Configs = new Dictionary<ConfigEntryBase, BepInConfigWrapper>();
+        internal readonly IDictionary<ConfigEntryBase, BepInConfigWrapper> Configs =
+            new Dictionary<ConfigEntryBase, BepInConfigWrapper>();
 
-        public ConfigBuilder WithCheckbox(ConfigEntry<bool> configEntry, string section = null, string label = null, string description = null)
+        public ConfigBuilder WithCheckbox(ConfigEntry<bool> configEntry, string section = null, string label = null,
+            string description = null)
         {
             var wrapper = new CheckboxConfigItem(configEntry);
             SetCustomAttributes(wrapper, section, label, description);
@@ -84,13 +94,72 @@ namespace IslandConfig
             return this;
         }
 
-        public ConfigBuilder WithDropdown<T>(ConfigEntry<T> configEntry, string section = null, string label = null, string description = null) where T : Enum
+        public ConfigBuilder WithDropdown<T>(ConfigEntry<T> configEntry, string section = null, string label = null,
+            string description = null) where T : Enum
         {
             var wrapper = new EnumDropdownConfigItem<T>(configEntry);
             SetCustomAttributes(wrapper, section, label, description);
             Configs[configEntry] = wrapper;
             return this;
         }
+
+        #region Numeric sliders
+
+        public ConfigBuilder WithSlider(ConfigEntry<byte> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new ByteSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        public ConfigBuilder WithSlider(ConfigEntry<short> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new ShortSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        public ConfigBuilder WithSlider(ConfigEntry<int> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new IntSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        public ConfigBuilder WithSlider(ConfigEntry<float> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new FloatSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        public ConfigBuilder WithSlider(ConfigEntry<double> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new DoubleSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        public ConfigBuilder WithSlider(ConfigEntry<decimal> configEntry, string section = null, string label = null,
+            string description = null)
+        {
+            var wrapper = new DecimalSliderConfigItem(configEntry);
+            SetCustomAttributes(wrapper, section, label, description);
+            Configs[configEntry] = wrapper;
+            return this;
+        }
+
+        #endregion
 
         private void SetCustomAttributes(BepInConfigWrapper wrapper, string section, string label, string description)
         {
