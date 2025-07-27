135bfb780965c8b706d9b87f28c7edce3a4623f8
csh <csh@smrkn.com>
2025-07-27 14:52:39 +0100

    Fix missing Initialize in CheckboxConfigItem.cs

--- Assets/Scripts/UI/CheckboxConfigItem.cs
+++ Assets/Scripts/UI/CheckboxConfigItem.cs
@@ -20,6 +20,7 @@ namespace IslandConfig.UI
             var prefab = Object.Instantiate(IslandConfigAssets.CheckboxPrefab);
 #endif
             var controller = prefab.GetComponent<CheckboxControllerScript>();
+            controller.Initialize(this, hoverNameTarget, hoverDescTarget);
             return prefab;
         }
     }
